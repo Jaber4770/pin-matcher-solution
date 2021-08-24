67dc3dba460ef48f75157d0a0b45c5aea6bfda2e
jaber4770 <jaber.ahmed4770@gmail.com>
2021-08-25 02:19:45 +0600

    working on progress

--- js/app.js
+++ js/app.js
@@ -15,6 +15,29 @@ function genaratePin(){
     document.getElementById('display-pin').value = pin;
 };
 
-document.getElementById('key-Pad').addEventListener('click', function(){
-    
+document.getElementById('key-Pad').addEventListener('click', function(event){
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-numbers');
+
+    if(isNaN(number)){
+        if(number == 'C'){
+            calcInput.value = '';
+        }
+    }
+    else{
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
 });
+
+function verifyPin(){
+    const pin = document.getElementById('display-pin').value;
+    const typed= document.getElementById('typed-numbers').value;
+    if(pin == typed){
+        console.log('yes you write the right pin.')
+    }
+    else{
+        console.log('sorry! you write the wrong number.')
+    }
+};
