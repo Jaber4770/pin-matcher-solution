29d1aa032200441e11518beba5b17a83d862e229
jaber4770 <jaber.ahmed4770@gmail.com>
2021-08-25 01:16:08 +0600

    current chole gesilo

---
+++ js/app.js
@@ -0,0 +1,20 @@
+function getPin (){
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+    if(pinString.length == 4){
+        return pin;
+    }
+    else{
+        // console.log('got 3 number pin.and return the function', pin);
+        return getPin();
+    }
+};
+
+function genaratePin(){
+    const pin = getPin();
+    document.getElementById('display-pin').value = pin;
+};
+
+document.getElementById('key-Pad').addEventListener('click', function(){
+    
+});
